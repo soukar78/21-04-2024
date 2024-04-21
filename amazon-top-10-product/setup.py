6a720920a32a3fb090414927a37d23d1ebbbe51c
soukar78 <soukar78@gmail.com>
2024-04-21 10:14:50 +0530

    Initial commit

---
+++ amazon-top-10-product/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This project help recommend the top 10 product items on Amazon home page.',
+    author='Sourav Karmakar',
+    license='',
+)
